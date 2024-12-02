aa4c167b4bf22aae0c28abe3b3950a97f48647de
Watson15 <aidanwatsonca15@gmail.com>
2024-12-02 11:47:51 -0800

    Added open source power dataset and a readme

--- Code/Joint Simulation/code/run_scenarios.bat
+++
@@ -1,5 +0,0 @@
-for /l %%i in (361, 1, 1000) do (
-   echo %%i
-   python32 one.py %%i
-   ping 127.0.0.1 -n 2 > nul
-)
--- Code/Joint Simulation/code/run_sspf.bat
+++
@@ -1,4 +0,0 @@
-for /l %%i in (1, 1, 100) do (
-   echo %%i
-   python32 ss_data.py
-)
